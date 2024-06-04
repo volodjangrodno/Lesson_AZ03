bc9214457dac66518c018c2801d7a324cb834410
volodjangrodno <parthnersclub.ru@gmail.com>
2024-06-04 16:48:39 +0300

    WD01dz

--- AZ03dz3.py
+++ AZ03dz3.py
@@ -1,25 +1,34 @@
+# 3. Необходимо спарсить цены на диваны с сайта divan.ru в csv файл, обработать данные,
+# найти среднюю цену и вывести ее, а также сделать гистограмму цен на диваны
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import pandas as pd
+from bs4 import BeautifulSoup
 import csv
-
+import matplotlib.pyplot as plt
+import scrapy
 
 driver = webdriver.Chrome()
-
-url = 'https://www.cian.ru/snyat-kvartiru-1-komn-ili-2-komn/'
+url = 'https://www.divan.ru/category/divany-i-kresla'
 driver.get(url)
 
 # Ожидание загрузки страницы
 time.sleep(5)  # Лучше использовать WebDriverWait для более надежного ожидания
 
+
 # Поиск элементов с ценами (замените селектор на актуальный)
-price_elements = driver.find_elements(By.XPATH, '//span[@data-mark="MainPrice"]/span')
+price_elements = driver.find_elements(By.XPATH, '//span[@data-testid="price"]/#text')
+
+# Закрытие веб-драйвера
+driver.quit()
 
 # Извлечение цен
 prices = [price.text for price in price_elements]
 
 # Сохранение в CSV файл
-with open('prices.csv', mode='w', newline='', encoding='utf-8') as file:
+with open('prices_Divans.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(['Price'])  # Заголовок столбца
 
@@ -28,6 +37,5 @@ with open('prices.csv', mode='w', newline='', encoding='utf-8') as file:
         writer.writerow([price])
 
 
-# Закрытие браузера
-driver.quit()
+
 
